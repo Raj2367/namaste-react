002bd9340357d20b3b873261bd0a146256ebc9e6
Pruthwiraj <pruthwirajnayak08@gmail.com>
2025-09-26 14:56:32 +0530

    useEffect used as componentWillUnmount

--- src/components/ContactUs.js
+++ src/components/ContactUs.js
@@ -1,4 +1,16 @@
+import { useEffect } from "react";
+
 const ContactUs = () => {
+  useEffect(() => {
+    const timer = setInterval(() => {
+      console.log("setinterval added");
+    }, 1000);
+
+    return () => {
+      clearInterval(timer);
+      console.log("clear inerval done");
+    };
+  }, []);
   return (
     <div>
       <h3>Contact Us page</h3>
