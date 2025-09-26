62bda46d3bc9eb00602fec29a89610479843feda
Pruthwiraj <pruthwirajnayak08@gmail.com>
2025-09-26 14:55:02 +0530

    class absed component with state added

--- src/components/AboutUs.js
+++ src/components/AboutUs.js
@@ -1,8 +1,27 @@
-const AboutUs = () => {
-  return (
-    <div>
-      <h3>About us page</h3>
-    </div>
-  );
-};
+import React from "react";
+class AboutUs extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      count: 1,
+      count2: 2,
+    };
+  }
+  render() {
+    const { name } = this.props;
+    return (
+      <div className="about-us">
+        <h3>{this.state.count} is the count</h3>
+        <h4>{name} is the name</h4>
+        <button
+          onClick={() => {
+            this.setState({ count: ++this.state.count });
+          }}
+        >
+          count++
+        </button>
+      </div>
+    );
+  }
+}
 export default AboutUs;
